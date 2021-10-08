c82a447048a389b8e2f71a996637245001d05456
trufan0ff <trufan.off@yandex.ru>
2021-10-08 16:34:33 +0300

    версия 0.1.2 после исправления

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,24 +6,17 @@ module.exports.getCards = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
 
-module.exports.getCurrentCard = (req, res) => {
-  Card.findById(req.params.id)
-    .then(card => res.status(200).send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }))
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  const owner = req.user._id;
+  return Card.create({ name, link, owner })
+    .then((card) => res.status(201).send({ data: card }))
+    .catch((e) => {
+      if (e.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Некорректные данные' });
+      } return res.status(500).send({ message: 'Произошла ошибка' });
+    })
 }
-
-module.exports.createCard = (req, res) => Card.findByIdAndRemove(req.params.id)
-.then((card) => {
-  if (!card) {
-    return res.status(404).send({ message: 'Карточка не найдена' });
-  } return res.status(200).send({ data: card });
-})
-.catch((e) => {
-  if (e.name === 'CastError') {
-    return res.status(400).send({ message: 'Некорректные данные' });
-  } return res.status(500).send({ message: 'Произошла ошибка' });
-})
-
 
 module.exports.deleteCard = (req, res) => Card.findByIdAndRemove(req.params.id)
   .then((card) => {
